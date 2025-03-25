e58690e2868da3ee40cd76ab2627130bb6f66454
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-26 01:55:43 +0600

    added tasksSlice to store reducer property

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import tasksSlice from "./features/tasks/tasksSlice";
 const store = configureStore({
-    reducer: {},
-})
+  reducer: {
+    tasks: tasksSlice,
+  },
+});
 
 export default store;
