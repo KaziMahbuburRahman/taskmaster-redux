d419a4a0809307461936ce002bdeaa7ff7a75ab9
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-30 01:10:20 +0600

    added userslice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksSlice from "./features/tasks/tasksSlice";
+import userSlice from "./features/tasks/userSlice";
 const store = configureStore({
   reducer: {
     tasks: tasksSlice,
+    users: userSlice,
   },
 });
 
