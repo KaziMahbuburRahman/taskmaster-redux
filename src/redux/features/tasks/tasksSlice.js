b7866fa56772b77448b650d71950a59f794c2dbb
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-29 21:47:16 +0600

    added task status and remove task function

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -8,14 +8,17 @@ const tasksSlice = createSlice({
   reducers: {
     addTask: (state, action) => {
       if (state.tasks.length === 0) {
-        state.tasks.push({ id: 1, ...action.payload });
+        state.tasks.push({ id: 1, status: "pending", ...action.payload });
       } else {
-        state.tasks.push({ id: state.tasks.at(-1).id + 1, ...action.payload });
+        state.tasks.push({ id: state.tasks.at(-1).id + 1, status: "pending", ...action.payload });
       }
+    },
+    removeTask: (state, action) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
   },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, removeTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
