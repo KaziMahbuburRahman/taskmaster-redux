7e7e9c457670549acf02e899b6904a347cbad718
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-29 21:09:23 +0600

    used index.scss instead of index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
-import routes from "./routes/routes.jsx";
+import "./index.scss";
 import store from "./redux/store.js";
+import routes from "./routes/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
