bdf73fab299c93e7cd952aeb09119df7420976a2
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-06 14:35:02 +0600

    added toaster to main.jsx to wrap all the components

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import "./index.scss";
 import store from "./redux/store.js";
 import routes from "./routes/routes.jsx";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <Toaster />
       <RouterProvider router={routes} />
     </Provider>
   </React.StrictMode>
