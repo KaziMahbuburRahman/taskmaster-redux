5f822e7c91ab0d37040a51bd9b98c1f391a01a8a
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-07 00:00:30 +0600

    integrated tasksApi into Redux store and updated middleware for enhanced API interaction

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { tasksApi } from "../features/tasks/tasksApiSlice";
 import tasksSlice from "./features/tasks/tasksSlice";
 import userSlice from "./features/tasks/userSlice";
 const store = configureStore({
   reducer: {
+    [tasksApi.reducerPath]: tasksApi.reducer,
     tasks: tasksSlice,
     users: userSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tasksApi.middleware),
 });
 
 export default store;
