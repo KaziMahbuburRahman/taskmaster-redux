db3d7426c50bb51cf9b51ce5d4e8e857cac0bda2
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-08 00:46:16 +0600

    refactored Login component for improved layout and styling, enhancing responsiveness and user experience with updated form structure, spacing, and text adjustments

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,16 +45,20 @@ const Login = () => {
   };
 
   return (
-    <div className="flex max-w-7xl h-screen items-center mx-auto">
-      <div className="w-1/2">
+    <div className="flex flex-col md:flex-row max-w-7xl min-h-screen items-center mx-auto p-4">
+      <div className="w-full md:w-1/2 hidden md:block">
         <img src={loginImage} className="h-full w-full" alt="" />
       </div>
-      <div className="w-1/2 grid place-items-center">
-        <div className="bg-primary/5 w-full max-w-sm rounded-lg grid place-items-center p-10">
-          <h1 className="mb-10 font-medium text-2xl">Login</h1>
+      <div className="w-full md:w-1/2 grid place-items-center">
+        <div className="bg-primary/5 w-full max-w-sm rounded-lg grid place-items-center p-6 md:p-10">
+          <h1 className="mb-6 md:mb-10 font-medium text-xl md:text-2xl">
+            Login
+          </h1>
           <form className="space-y-3 w-full" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col items-start">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email" className="mb-1">
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
@@ -63,7 +67,9 @@ const Login = () => {
               />
             </div>
             <div className="flex flex-col items-start">
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password" className="mb-1">
+                Password
+              </label>
               <input
                 type="password"
                 id="password"
@@ -71,12 +77,12 @@ const Login = () => {
                 {...register("password")}
               />
             </div>
-            <div className="relative !mt-8">
+            <div className="relative !mt-6 md:!mt-8">
               <button type="submit" className="btn btn-primary w-full">
                 Login
               </button>
             </div>
-            <div>
+            <div className="text-sm md:text-base">
               <p>
                 Don&apos;t have an account?{" "}
                 <span
