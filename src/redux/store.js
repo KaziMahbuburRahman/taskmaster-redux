34825485f7e0b323b7d54f6d02f6c6964faecdb6
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-08 01:12:35 +0600

    added userImagesApi to Redux store, enhancing state management for user images and improving middleware integration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { tasksApi } from "./features/api/tasksApiSlice";
+import { userImagesApi } from "./features/api/userImagesApi";
 import tasksSlice from "./features/tasks/tasksSlice";
 import userSlice from "./features/tasks/userSlice";
 import usersSlice from "./features/tasks/usersSlice";
@@ -8,12 +9,16 @@ import usersSlice from "./features/tasks/usersSlice";
 const store = configureStore({
   reducer: {
     [tasksApi.reducerPath]: tasksApi.reducer,
+    [userImagesApi.reducerPath]: userImagesApi.reducer,
     tasks: tasksSlice,
     users: userSlice,
     allUsers: usersSlice,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tasksApi.middleware),
+    getDefaultMiddleware().concat(
+      tasksApi.middleware,
+      userImagesApi.middleware
+    ),
 });
 
 export default store;
