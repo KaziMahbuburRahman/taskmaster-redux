ba290b69e39e95793955c2374f4f4418141e2046
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-07 17:57:47 +0600

    added usersSlice to Redux store for managing all users, enhancing state management alongside existing tasks and user slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { tasksApi } from "./features/api/tasksApiSlice";
 import tasksSlice from "./features/tasks/tasksSlice";
 import userSlice from "./features/tasks/userSlice";
-import { tasksApi } from "./features/api/tasksApiSlice";
+import usersSlice from "./features/tasks/usersSlice";
+
 const store = configureStore({
   reducer: {
     [tasksApi.reducerPath]: tasksApi.reducer,
     tasks: tasksSlice,
     users: userSlice,
+    allUsers: usersSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tasksApi.middleware),
