292d0a483f8a1e1ca9e6edf4a6eb5ba1badd920d
NoobMahbub <Mahbubbengali@gmail.com>
2025-04-08 04:25:26 +0600

    replaced Profile component content with ComingSoon placeholder, indicating upcoming features and improving user experience

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,7 @@
+import ComingSoon from "../components/shared/ComingSoon";
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile </h1>
-    </div>
-  );
+  return <ComingSoon />;
 };
 
 export default Profile;
